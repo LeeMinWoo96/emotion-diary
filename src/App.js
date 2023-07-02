f59498e5038dc884bfdfe5e1af0249c380081c6a
minwoo <dlworl207@gmail.com>
2023-07-02 18:52:33 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-import Home from './pages/Home'
+import Home from './pages/Home';
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
+import RouteTest from "./components/RouteTest";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
                 <h2>App.js</h2>
+
+
+                {/*<Routes>*/}
+                {/*    <Route path='/' element={<Home />}></Route>*/}
+                {/*    <Route path='/new' element={<New />}></Route>*/}
+                {/*    <Route path='/edit' element={<Edit />}></Route>*/}
+                {/*    <Route path='/diary/:id' element={<Diary />}></Route>*/}
+                {/*</Routes>*/}
+                {/*/!*아래처럼 하면 이동할때마다 새로고침함 --> spa 방식의 장점아 아님 // 이걸 다중 페이지 방식에서나*!/*/}
+                {/*<a href={"/new"}> New 로이동</a>*/}
+                {/*<br/>*/}
+                {/*<RouteTest />*/}
             </div>
         </BrowserRouter>
     );
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,7 +1,21 @@
+import {useNavigate, useSearchParams} from "react-router-dom";
+
 const Edit = () =>{
+    const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const id = searchParams.get('id');
+    const mode = searchParams.get('mode');
+    console.log(mode)
     return <div>
         <h1>Edit</h1>
         <p>이곳은 수정 페이지입니다.</p>
+        <button onClick={()=> setSearchParams({who :"minwoo"})}>QS 바꾸기</button>
+
+        {/*링크를 누르지 않아도 navigate를 이용해서 강제로 페이지 이동시킬 수 있음*/}
+        {/*로그인 안한 애가 접근하거나 할떄 쓰임*/}
+        <button onClick={()=>navigate('/')}>Home 으로 가기</button>
+        <button onClick={()=>navigate('-1')}>뒤로 가기</button>
+
     </div>
 };
 export default Edit;
